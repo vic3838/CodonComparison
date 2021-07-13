30a00d0a782e6b7700e120d5bc4c63ada488144c
Vijay kiran <vic38@pitt.edu>
2021-07-13 13:02:32 -0400

    test

---
+++ src/CodonComparison.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class CodonComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		getEmergingORFs();
+		
+		
+	}
+	
+	
+	public static void getEmergingORFs()
+	{
+		
+	}
+	
+}
